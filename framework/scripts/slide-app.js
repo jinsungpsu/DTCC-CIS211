2ae0f68cba97a26d057d082713277988f4fd9f1d
Jin An <jinsungan@gmail.com>
2026-08-20 09:05:20 -0400

    Update framework

--- framework/scripts/slide-app.js
+++ framework/scripts/slide-app.js
@@ -192,11 +192,6 @@ ${markdown}
                 customcontrols: {
                     controls: [
                         {
-                            icon: '<i class="fa fa-pen-square"></i>',
-                            title: 'Toggle chalkboard (B)',
-                            action: 'RevealChalkboard.toggleChalkboard();'
-                        },
-                        {
                             icon: '<i class="fa fa-pen"></i>',
                             title: 'Toggle notes canvas (C)',
                             action: 'RevealChalkboard.toggleNotesCanvas();'
